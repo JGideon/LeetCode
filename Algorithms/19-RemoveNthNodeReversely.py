f79901f94c3361995df110a4d44ddb9d019a8922
陈岳燊 <chenys4@gmail.com>
2015-05-16 23:25:03 +0800

    Day 2

---
+++ Algorithms/19-RemoveNthNodeReversely.py
@@ -0,0 +1,24 @@
+#encoding=utf-8
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    '''
+    @param ListNode head
+    @param integer n
+    @return ListNode
+    '''
+    def removeNthFromEnd(self, head, n):
+        dummyHead = ListNode(0)
+        dummyHead.next = head
+        slow = quick = dummyHead.next
+        for i in range(n):
+            quick = quick.next
+        while quick.next:
+            slow = slow.next
+            quick = quick.next
+        slow.next = slow.next.next 
+        return head
+            
